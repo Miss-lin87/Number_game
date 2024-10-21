b0a53ac5ccb1082dd935d1ac5126e1f1827f8e38
linda <linda.ek@bredband2.com>
2024-10-21 20:39:28 +0200

    Final commit, fixed minor stuff and exceptions.

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import number_game
 from random import randint as random
 
+# A tracker keeping track on the number of tries made
 tries = 1
 
+# this runs the game
 def game():
-    number_game.start()
-    global tries
-    ran_low, ran_hi = number_game.rang()
-    target = random(ran_low, ran_hi)
-    print("The computer has picked a target number")
-    guess = number_game.comparison(target, number_game.test_valid(ran_low, ran_hi, number_game.guesses()))
-    while guess != target:
-        tries += 1
-        guess = int(number_game.comparison(target, number_game.test_valid(ran_low, ran_hi, number_game.guesses())))
-    else: return "It took you " + str(tries) + " tries to guess correct"
+    try:
+        number_game.start()
+        global tries
+        ran_low, ran_hi = number_game.rang()
+        target = random(ran_low, ran_hi)
+        print("The computer has picked a target number")
+        guess = number_game.comparison(target, number_game.test_valid(ran_low, ran_hi, number_game.guesses()))
+        while guess != target:
+            tries += 1
+            guess = int(number_game.comparison(target, number_game.test_valid(ran_low, ran_hi, number_game.guesses())))
+        else: return "It took you " + str(tries) + " tries to guess correct"
+    except ValueError:
+        print("Something went wrong. Starting over\n\n")
+        game()
 
 print(game())
---
+++ test_I.py
@@ -0,0 +1,2 @@
+import pytest
+import number_game
